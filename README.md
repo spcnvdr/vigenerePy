94ec960744067e4224935e1210e19c6ace17b845
Bryan Hawkins <spcnvdrr@protonmail.com>
2018-07-14 09:09:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# VigenerePy - A simple Python 3.x class which implements the Vigenere cipher
+
+The Vigenere cipher is a simple polyalphabetic substitution cipher
+meant to secure messages with a password or keyword. According *Principles of
+Information Security* by Whitman and Mattord, "The cipher is implemented
+using the Vigenere square (or table), also known as a tabula recta - a term
+invented by Johannes Trithemius in the 1500s." The Vigenere square is made up
+of 26 distinct cipher alphabets. In the header row and column, the alphabet is
+written in its normal order. In each subsequent row, the alphabet is shifted
+one letter to the right until a 26 x 26 block of letters is formed. This
+implementation uses a keyword, or password, to represent the shift for each
+letter.
+
+
+
+This project is a simple implementation of the Vigenere Cipher as a Python 3.x
+class. It is meant to be easy to use, read, and modify. Valid passwords must
+be at least one letter long and contain only letters. Numbers and symbols are
+ignored. Also, passwords are case insensitive. Symbols and numbers
+(including spaces) are ignored and removed from the ciphertext.  
+
+
+**Available Methods**
+
+* vigenere.Vigenere(string) - Return a vigenere object using string as the password
+
+* vigenere.encrypt(string) - Encrypt the given string with the Vignere Cipher
+
+* vigenere.decrypt(string) - Decrypt the given string with the Vigenere Cipher
+
+* vigenere.setPassword(pass) - Change the current password/keyword to pass
+
+**Example**
+
+    from vigenere import Vigenere
+    v = Vigenere("Password")
+    c = v.encrypt("Hello world!")
+    p = v.decrypt(c)
+    print("Ciphertext: ", c)
+    print("Plaintext: ", p)
+
+Which outputs:
+
+    Ciphertext:  WEDDKKFUAD
+    Plaintext:  HELLOWORLD
+
+
+**Contributing**
+
+Pull requests, new feature suggestions, and bug reports/issues are
+welcome.
+
+
+**Versioning**
+
+This project uses semantic versioning 2.0. Version numbers follow the
+MAJOR.MINOR.PATCH format.
+
+
+**License**
+
+This project is licensed under the 3-Clause BSD License also known as the
+*"New BSD License"* or the *"Modified BSD License"*. A copy of the license
+can be found in the LICENSE file. A copy can also be found at the
+[Open Source Institute](https://opensource.org/licenses/BSD-3-Clause)
---
+++ vigenere.py
@@ -0,0 +1,123 @@
+# Title:       vigenere.py
+# Version:     1.0.0
+# Description: A simple class that implements the Vigenere cipher.
+# Note: Valid Passwords MUST be at least one letter long. Otherwise the class
+# will throw an error. Numbers and symbols are not valid characters in
+# passwords and are ignored.
+
+class Vigenere:
+    ## Initialize the Vigenere object
+    #  @param password a string to use as a password
+    def __init__(self, password = "Hello world"):
+        # create a variable to hold the password and set it
+        self.vigPass = ""
+        self.setPassword(password)
+
+
+    ## Takes a letter from the user and turns it into a number between 0 and 25 to use
+    #  as a key for encryption or decryption
+    #  @param char a character to use as a key
+    #  @return charKey an integer between 0 and 25
+    #
+    def ChToKey(self, char):
+        if char.islower():
+            char = char.upper()
+
+        if char >= "A" and char <= "Z":
+            offset = ord("A")
+        else:
+            # Not a letter
+            return None
+
+        charKey = ord(char) - offset
+        return charKey
+
+
+    ## Removes all characters not found in the alphabet from an input string and
+    #  converts all characters  to uppercase.
+    #  @param text the string to be converted
+    #  @return newText The new all uppercase, alpha only string.
+    #
+    def convertStr(self, text):
+        newText = ""
+        for char in text:
+            if char.isalpha():
+                newText += char.upper()
+
+        return newText
+
+
+    ## Encrypts upper- and lowercase characters by shifting them according to a key.
+    #  @param ch the letter to be encrypted
+    #  @param key the encryption key
+    #  @param mode a character that determines the operation E for encryption, and
+    #   D for decryption
+    #  @return the encrypted letter
+    #
+    def crypt(self, ch, key, mode):
+        LETTERS = 26   #Number of letters in the Roman alphabet
+
+        if ch >= "A" and ch <= "Z":
+            base = ord("A")
+        else:
+            return ch   # Catch-all: Not an uppercase letter
+
+        if mode.upper() == "E":
+            offset = ord(ch) - base + key
+        elif mode.upper() == "D":
+            offset = ord(ch) - base - key
+
+        if offset >= LETTERS:
+            offset = offset - LETTERS
+        elif offset < 0:
+            offset = offset + LETTERS
+
+        return chr(base + offset)
+
+
+    ## Decrypts a message with the Vigenere cipher
+    #  @param message the string to decrypt
+    #  @return the decrypted string
+    #
+    def decrypt(self, message):
+        tempMes = self.convertStr(message)
+        newMes = ""
+        tempKey = 0
+        j = 0
+
+        for char in tempMes:
+            tempKey = self.ChToKey(self.vigPass[j % len(self.vigPass)])
+            newMes += self.crypt(char, tempKey, "D")
+            j += 1
+
+
+        return newMes
+
+    ## Encrypts a message with the Vigenere cipher
+    #  @param message the string to decrypt
+    #  @return the encrypted string
+    #
+    def encrypt(self, message):
+        tempMes = self.convertStr(message)
+        newMes = ""
+        tempKey = 0
+        j = 0
+
+        for char in tempMes:
+            tempKey = self.ChToKey(self.vigPass[j % len(self.vigPass)])
+            newMes += self.crypt(char, tempKey, "E")
+            j += 1
+
+        return newMes
+
+
+    ## Sets a new password to be used for encryption/decryption
+    #  @param newPass a string to be used as the password
+    #
+    def setPassword(self, newPass):
+        tmp = self.convertStr(newPass)
+        if len(tmp) == 0:
+            raise IndexError("Error: Bad password. Password must contain only letters")
+        else:
+            self.vigPass = tmp
+            #print(self.vigPass)
